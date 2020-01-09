f106520c24417bbb2e546bb914c162da3d1d7223
Rajdeep Kaur <chahal.rajdeep03@gmail.com>
2020-01-08 20:38:38 -0500

    square moving

--- Squaremoving/ViewController.swift
+++ Squaremoving/ViewController.swift
@@ -10,11 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var Square: UIView!
+    
+    
+    var timer:Timer?
+        var Top = false
+        var Bottom = true
+        let screenWidth = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            startTimer()
+            let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+            self.view.addGestureRecognizer(tap)
+        }
+        
+        @objc func btnUp() {
+            var xPosition = Square.frame.origin.x
+            var yPosition = Square.frame.origin.y
+            
+            if Bottom {
+                yPosition += 20
+            } else if Top {
+                yPosition -= 20
+            }
+            
+            if yPosition > screenHeight {
+                xPosition = generateRandomXCordinate()
+                yPosition = screenHeight - 70
+                Top = true
+                Bottom = false
+            } else if yPosition < 0 {
+                xPosition = generateRandomXCordinate()
+                yPosition +=  70
+                Top = false
+                Bottom = true
+            }
+
+            let width = Square.frame.size.width
+            let height = Square.frame.size.height
+
+            UIView.animate(withDuration: 1.0, animations: {
+                self.Square.frame = CGRect(x: xPosition, y: yPosition, width: width, height: height)
+            })
+        }
+        
+        func generateRandomXCordinate() -> CGFloat{
+            let randomInt = Int.random(in: 1..<Int(screenWidth - 30))
+            return CGFloat(randomInt)
+        }
+        func startTimer() {
+            timer = Timer.scheduledTimer(timeInterval: 0.3,
+            target: self,
+            selector: #selector(btnUp),
+            userInfo: nil,
+            repeats: true)
+        }
+        func stopTimer() {
+            if timer != nil {
+                timer?.invalidate()
+                timer = nil
+            }
+        }
+        @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+            if timer != nil {
+                stopTimer()
+            } else {
+                startTimer()
+            }
+        }
+
+
     }
 
-
-}
 
